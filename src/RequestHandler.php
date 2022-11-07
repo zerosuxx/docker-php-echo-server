48652d2d863f5f3a384842496bb007475ab7e0f2
Tamas Mohos <zerosuxx@gmail.com>
2022-11-07 13:16:09 +0100

    restructure actions

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -32,45 +32,10 @@ class RequestHandler
 
     public function handle(Request $request, Response $response): void
     {
-        echo $this->jsonEncode([
-            'version' => $this->version,
-            'request' => $request,
-            'server' => $this->server,
-            'stats' => $this->server->stats(),
-            'connection_info' => $this->server->connection_info($request->fd),
-            'env' => $this->env
-        ]) . PHP_EOL;
-
         $response->header('Server', "EchoServer ($this->version)");
-
-        $hostName = gethostname();
-        $responseBody = [
-            'VERSION' => $this->version,
-            'HOST_NAME' => $hostName,
-            'NODE_NAME' => $this->env['NODE_NAME'] ?? $hostName,
-            'USER_AGENT' => $request->header['user-agent'],
-            'REMOTE_IP' => $request->server['remote_addr'],
-            'PROTOCOL' => $request->server['server_protocol'],
-            'URI' => $request->server['request_uri'],
-            'METHOD' => $request->server['request_method'],
-            'ADDITIONAL_HEADERS' => $this->getAdditionalHeaders($request->header),
-            'COOKIE' => $request->cookie ?? [],
-            'QUERY' => $request->get ?? [],
-            'PARSED_BODY' => $request->post ?? [],
-            'RAW_BODY' => $request->getContent()
-        ];
-
-        $debugMode = (bool)($request->header['debug'] ?? false);
-        if ($debugMode) {
-            $responseBody['SERVER'] = $request->server;
-            $responseBody['ENV'] = $this->env;
-        }
-
         $response->header('Content-Type', 'application/json');
 
         $this->handleActions($request, $response);
-
-        $response->end($this->jsonEncode($responseBody));
     }
 
     private function getAdditionalHeaders(array $headers): array
@@ -90,7 +55,9 @@ class RequestHandler
     private function handleActions(Request $request, Response $response): void
     {
         $requestUri = $request->server['request_uri'];
-        if (preg_match('#/status/(.+)#', $requestUri, $statusMatches)) {
+        if ($requestUri === '/healthcheck') {
+            $response->end($this->jsonEncode(['success' => true]));
+        } else if (preg_match('#/status/(.+)#', $requestUri, $statusMatches)) {
             if ($statusMatches[1] === 'random') {
                 $availableStatuses = [200, 401, 500];
                 $statusCode = $availableStatuses[array_rand($availableStatuses)];
@@ -100,6 +67,40 @@ class RequestHandler
             $response->status($statusCode);
         } else if (preg_match('#/redirect/(.+)#', $requestUri, $redirectMatches)) {
             $response->redirect($redirectMatches[1]);
+        } else {
+            echo $this->jsonEncode([
+                'version' => $this->version,
+                'request' => $request,
+                'server' => $this->server,
+                'stats' => $this->server->stats(),
+                'connection_info' => $this->server->connection_info($request->fd),
+                'env' => $this->env
+            ]) . PHP_EOL;
+            
+            $hostName = gethostname();
+            $responseBody = [
+                'VERSION' => $this->version,
+                'HOST_NAME' => $hostName,
+                'NODE_NAME' => $this->env['NODE_NAME'] ?? $hostName,
+                'USER_AGENT' => $request->header['user-agent'],
+                'REMOTE_IP' => $request->server['remote_addr'],
+                'PROTOCOL' => $request->server['server_protocol'],
+                'URI' => $request->server['request_uri'],
+                'METHOD' => $request->server['request_method'],
+                'ADDITIONAL_HEADERS' => $this->getAdditionalHeaders($request->header),
+                'COOKIE' => $request->cookie ?? [],
+                'QUERY' => $request->get ?? [],
+                'PARSED_BODY' => $request->post ?? [],
+                'RAW_BODY' => $request->getContent()
+            ];
+
+            $debugMode = (bool)($request->header['debug'] ?? false);
+            if ($debugMode) {
+                $responseBody['SERVER'] = $request->server;
+                $responseBody['ENV'] = $this->env;
+            }
+
+            $response->end($this->jsonEncode($responseBody));
         }
     }
 
